73c9d35982de23e9058f6ff5ec0930a05dc4b3c4
keibn29 <keibn29@gmail.com>
2024-04-19 16:07:17 +0700

    [UPDATE] folders constructure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,16 @@ import { JwtModule } from '@nestjs/jwt';
 import { MulterModule } from '@nestjs/platform-express';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { AuthModule } from './auth/auth.module';
-import { JwtStategy } from './auth/strategy';
-import { BlogModule } from './blog/blog.module';
-import { BookingModule } from './booking/booking.module';
-import { BranchModule } from './branch/branch.module';
-import { GalleryModule } from './gallery/gallery.module';
+import { AuthModule } from './components/auth/auth.module';
+import { JwtStategy } from './components/auth/strategy';
+import { BlogModule } from './components/blog/blog.module';
+import { BookingModule } from './components/booking/booking.module';
+import { BranchModule } from './components/branch/branch.module';
+import { GalleryModule } from './components/gallery/gallery.module';
+import { ScheduleModule } from './components/schedule/schedule.module';
+import { UserModule } from './components/user/user.module';
+import { UtilityModule } from './components/utility/utility.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
-import { UtilityModule } from './utility/utility.module';
-import { ScheduleModule } from './schedule/schedule.module';
 
 @Module({
   imports: [
